f2dcc2ee082d4868c817878d794ccc65d2ba8501
chundong yang <476210790@qq.com>
2017-08-21 18:12:35 +0800

    the first

---
+++ app/src/main/java/ptra/hacc/cc/pulltorefreshattacher/MainActivity.java
@@ -0,0 +1,18 @@
+package ptra.hacc.cc.pulltorefreshattacher;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Hale Yang on 2017/8/16.
+ *
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/ptra/hacc/cc/pulltorefreshattacher/PullToRefershAttacher.java
@@ -0,0 +1,101 @@
+package ptra.hacc.cc.pulltorefreshattacher;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.NestedScrollingChild;
+import android.support.v4.view.NestedScrollingChildHelper;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by Hale Yang on 2017/8/16.
+ * 此乃下拉刷新容器
+ */
+
+public class PullToRefershAttacher extends RelativeLayout implements NestedScrollingChild{
+
+    private NestedScrollingChildHelper mScrollingChildHelper;
+
+    public PullToRefershAttacher(@NonNull  Context context, @NonNull View view){
+        super(context);
+    }
+
+    private PullToRefershAttacher(Context context) {
+        super(context);
+    }
+
+    private PullToRefershAttacher(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @TargetApi(21)
+    private PullToRefershAttacher(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(21)
+    private PullToRefershAttacher(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    /**
+     * 设置是否可以进行嵌套滚动
+     * @param enabled true or false
+     */
+    @Override
+    public void setNestedScrollingEnabled(boolean enabled) {
+        getScrollingChildHelper().setNestedScrollingEnabled(enabled);
+    }
+
+
+    @Override
+    public boolean isNestedScrollingEnabled(){
+        return getScrollingChildHelper().isNestedScrollingEnabled();
+    }
+
+    @Override
+    public boolean startNestedScroll(int axes) {
+        return getScrollingChildHelper().startNestedScroll(axes);
+    }
+
+    @Override
+    public void stopNestedScroll() {
+        getScrollingChildHelper().stopNestedScroll();
+    }
+
+    @Override
+    public boolean hasNestedScrollingParent() {
+        return getScrollingChildHelper().hasNestedScrollingParent();
+    }
+
+    @Override
+    public boolean dispatchNestedScroll(int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int[] offsetInWindow) {
+        return getScrollingChildHelper().dispatchNestedScroll(dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, offsetInWindow);
+    }
+
+    @Override
+    public boolean dispatchNestedPreScroll(int dx, int dy, int[] consumed, int[] offsetInWindow) {
+        return getScrollingChildHelper().dispatchNestedPreScroll(dx, dy, consumed, offsetInWindow);
+    }
+
+    @Override
+    public boolean dispatchNestedFling(float velocityX, float velocityY, boolean consumed) {
+        return getScrollingChildHelper().dispatchNestedFling(velocityX, velocityY, consumed);
+    }
+
+    @Override
+    public boolean dispatchNestedPreFling(float velocityX, float velocityY) {
+        return getScrollingChildHelper().dispatchNestedPreFling(velocityX, velocityY);
+    }
+
+    private NestedScrollingChildHelper getScrollingChildHelper() {
+        if (mScrollingChildHelper == null) {
+            mScrollingChildHelper = new NestedScrollingChildHelper(this);
+        }
+        return mScrollingChildHelper;
+    }
+
+
+}
